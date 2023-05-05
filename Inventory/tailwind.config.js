c12c3d0d6ffa072cdefe49bd2a4e1639bb37b1bc
Chris Alcantara <christian_pria@hotmail.com>
2023-05-05 14:52:05 -0600

    preline Discarded

---
+++ Inventory/tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: ["./src/**/*.{html,js}",
+  'node_modules/preline/dist/*.js',
+  'node_modules/preline/dist/preline.js'],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('preline/plugin'),
+    require('@tailwindcss/forms'),],
+}
+
